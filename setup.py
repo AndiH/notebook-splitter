f18cca2fe3674d1a32843867ea1e8f9aa214e50c
Andreas Herten <a.herten@fz-juelich.de>
2019-03-08 16:39:10 +0100

    Parse description von README.md

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='notebook_splitter',
-      version='1.1',
+      version='1.2',
       description='Jupyter Notebook Splitter',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/AndiH/notebook-splitter',
       author='Andreas Herten',
       author_email='a.herten@gmail.com',
